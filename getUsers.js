39af8d9ba5b9e53a763c457f697abd1d839b1315
Matthew Bebis <matthewbebis@gmail.com>
2019-04-03 21:57:21 +0000

    getUsers search refined to users withover 204 followers and extended to a search of 100 per page, added page to get req

--- getUsers.js
+++ getUsers.js
@@ -3,18 +3,21 @@ const fs = require('file-system')
 
 arr = []
 
-fetch('https://api.github.com/search/users?q=location:toronto+followers:%3E204&page=1&per_page=100', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
+fetch(
+  'https://api.github.com/search/users?q=location:toronto+followers:%3E204&page=1&per_page=100',
+  {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
   }
-})
+)
   .then(res => res.json())
   .then(json => {
     json.items.forEach(element => {
       arr.push(JSON.stringify(element.login))
     })
     console.log(arr)
-    fs.writeFile('users.json', '{\"users\":[' + arr + ']}', function(err) {})
+    fs.writeFile('users.json', '{"users":[' + arr + ']}', function(err) {})
   })
   .catch(err => console.error(err))
