830e3c26f0b6170f113dda57055741cd298b2716
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-04-03 12:43:27 -0400

    fmt

--- all-time.js
+++ all-time.js
@@ -6,23 +6,25 @@ const dbName = 'devexplorer'
 
 const work = client => {
   const db = client.db(dbName)
-  const collection = db.collection('users');
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ]).then(result => {
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    return collection.find({}).toArray()
-  }).then((docs) => {
-    console.log("Found the following records");
-    console.log(docs)
-    return collection.find({'a': 3}).toArray()
-  }).then((docs) => {
-    console.log("Found the following records");
-    console.log(docs);
-    client.close()
-  });
+  const collection = db.collection('users')
+  collection
+    .insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+    .then(result => {
+      assert.equal(3, result.result.n)
+      assert.equal(3, result.ops.length)
+      console.log('Inserted 3 documents into the collection')
+      return collection.find({}).toArray()
+    })
+    .then(docs => {
+      console.log('Found the following records')
+      console.log(docs)
+      return collection.find({ a: 3 }).toArray()
+    })
+    .then(docs => {
+      console.log('Found the following records')
+      console.log(docs)
+      client.close()
+    })
 }
 
 MongoClient.connect(url)
--- index.js
+++ index.js
@@ -16,7 +16,6 @@ MongoClient.connect(url, (err, client) => {
 
 const setup = client => {
   const db = client.db(dbName)
-  app.get('/', (req, res) => 
-    res.sendFile(__dirname +'/views/index.html'))
+  app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }
