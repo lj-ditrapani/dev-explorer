a220be119745c25482f91dd1ace73280038226af
billwu99 <billwu95@gmail.com>
2019-04-03 15:10:36 -0400

    we can hit v4

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const url = 'mongodb://localhost:27017'
 const dbName = 'devexplorer'
 
 MongoClient.connect(url, (err, client) => {
-    assert.equal(null, err)
-    console.log('Connected to mongodb')
-    setup(client)
+  assert.equal(null, err)
+  console.log('Connected to mongodb')
+  setup(client)
 })
 
 const setup = client => {
-    const db = client.db(dbName)
-    app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  const db = client.db(dbName)
+  app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }
--- loadData.js
+++ loadData.js
@@ -1,10 +1,21 @@
 const axios = require('axios')
 
-axios.post('https://api.github.com/graphql', {
-    query: `query {viewer {login}}`,
-    header: `Authorization: bearer ${process.env.token}`
-}).then(res => {
-    console.log("success: ", res.data)
-}).catch(err => {
-    console.log("error: ", err)
-})
+axios
+  .post(
+    'https://api.github.com/graphql',
+    {
+      query: `query {viewer {login}}`
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${process.env.token}`
+      }
+    }
+  )
+  .then(res => {
+    console.log('success: ', res.data)
+  })
+  .catch(err => {
+    console.log('error: ', err)
+  })
