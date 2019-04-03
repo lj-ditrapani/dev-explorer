ca824e3736a306fc164eff98e97788917691e9ae
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-04-03 10:44:42 -0400

    Organize index

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express')
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
 
 const app = express()
 const port = 3000
  
-const url = 'mongodb://localhost:27017';
-const dbName = 'devexplorer';
+const url = 'mongodb://localhost:27017'
+const dbName = 'devexplorer'
  
-MongoClient.connect(url, function(err, client) {
+MongoClient.connect(url, (err, client) => {
   assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
+  console.log("Connected to mongodb")
+  setup(client)
+})
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const setup = (client) => {
+  const db = client.db(dbName)
+  app.get('/', (req, res) => res.send('Hello World!'))
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
