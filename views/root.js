c0ffa523232e8137ccf187d4fe4d7f2d6035e5a7
billwu99 <billwu95@gmail.com>
2019-04-04 13:11:29 -0400

    stuff works

--- views/root.js
+++ views/root.js
@@ -1,86 +1,5 @@
 // var DATA = require('./constants');
 
-const cities = [
-  {
-    name: 'Toronto',
-    language: 'Hi',
-    numberOfUser: 0
-  },
-  {
-    name: 'Montreal',
-    language: 'Hi',
-    numberOfUser: 0
-  },
-  {
-    name: 'Waterloo',
-    language: 'Hi',
-    numberOfUser: 0
-  }
-]
-
-const languages = [
-  {
-    name: 'Java',
-    img: 'http://www.sclance.com/pngs/java-png/java_png_728079.png'
-  },
-  {
-    name: 'JavaScript',
-    img:
-      'https://banner2.kisspng.com/20180810/fvl/kisspng-javascript-comment-html-logo-international-confere-amp-quot-need-page-amp-quot-5b6d61dfbbdf29.2420070415338951357695.jpg'
-  },
-  {
-    name: 'Python',
-    img: 'https://pluralsight.imgix.net/paths/python-7be70baaac.png'
-  },
-  {
-    name: 'CSS',
-    img: 'https://cdn-images-1.medium.com/max/1200/1*eXIBeNlLhz4Pe6vDrYkXLQ.png'
-  },
-  {
-    name: 'HTML',
-    img: 'https://ya-webdesign.com/images/svg-sites-html-5.png'
-  },
-  {
-    name: 'SHELL',
-    img: 'http://www.stickpng.com/assets/images/5954be4edeaf2c03413be356.png'
-  },
-  {
-    name: 'C++',
-    img: 'https://raw.githubusercontent.com/isocpp/logos/master/cpp_logo.png'
-  },
-  {
-    name: 'C',
-    img:
-      'https://seeklogo.com/images/C/c-programming-language-logo-9B32D017B1-seeklogo.com.png'
-  }
-]
-const citiesData = [
-  {
-    topLanguage: 'Java',
-    users: 1000,
-    bitSize: 6969696
-  },
-  {
-    topLanguage: 'Java',
-    users: 1000,
-    bitSize: 6969696
-  },
-  {
-    topLanguage: 'Java',
-    users: 1000,
-    bitSize: 6969696
-  },
-  {
-    topLanguage: 'Java',
-    users: 1000,
-    bitSize: 6969696
-  },
-  {
-    topLanguage: 'Java',
-    users: 1000,
-    bitSize: 6969696
-  }
-]
 const getCities = () => {
   return $.get('http://localhost:3000/cities')
     .done(cities => {
@@ -110,7 +29,7 @@ const getUsers = () => {
 }
 const getUser = user => {
   return $.get(`http://localhost:3000/user/${user}`)
-    .done((res) => {
+    .done(res => {
       console.log('successfully retrieved user data')
     })
     .fail(function() {
@@ -118,40 +37,30 @@ const getUser = user => {
     })
 }
 
-const mapLanguGeImgs = () => {
-  citiesData.map(i => {
-    languages.map(j => {
-      if (i.topLanguage === j.name) {
-        i.img = j.img
-      }
-    })
-  })
-}
-
 $(document).ready(function() {
   console.log('ready!')
   initMap()
-  displayMarkers(cities)
+  displayMarkers(getCities())
   addCities()
   addUsers()
-  mapLanguGeImgs()
   $('.collapsible').collapsible()
 })
 
 const addCities = e => {
-  cities.map(item => {
-    $('#cities').append(
-      `<li class="collection-item">${item.name}</li>
-            <li class="collection-item">${item.numberOfUser}</li>
+  getCities().done(res => {
+    res.cities.map(item => {
+      $('#cities').append(
+        `<li class="collection-item">${item.location}</li>
+            <li class="collection-item">${item.users}</li>
             <li class="collection-item">${item.language}</li>
             `
-    )
+      )
+    })
   })
 }
 
 const addUsers = () => {
   getUsers().done(res => {
-      console.log(res)
     res.users.forEach(user => {
       getUser(user).then(item => {
         $('#collapsible').append(
@@ -192,9 +101,9 @@ function initMap() {
 }
 
 const getLocationsCoords = (location, topLanguage, numUsers) => {
-    if (!location.includes(',')) {
-      location+=(', On')
-    }
+  if (!location.includes(',')) {
+    location += ', On'
+  }
   fetch(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyDIugBS6uYAKtUfNwqn7gqL6JnlIpAKeSQ`
   )
@@ -218,13 +127,11 @@ const getLocationsCoords = (location, topLanguage, numUsers) => {
     .catch(err => console.error(err))
 }
 
-const users = [
-  
-]
-
-const displayMarkers = (cities) => {
-  cities.forEach(city => {
-    getLocationsCoords(city.name, city.language, city.numberOfUser)
+const displayMarkers = cities => {
+  cities.done(res => {
+    res.cities.forEach(city => {
+      getLocationsCoords(city.location, city.topLanguage, city.numUsers)
+    })
   })
 }
 
