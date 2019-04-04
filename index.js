7c52aaadff9830b0e06d8b3599fdfeca3437b4fe
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-04-04 08:22:22 -0400

    Create skeleton endpoints

--- index.js
+++ index.js
@@ -16,12 +16,27 @@ const setup = client => {
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
-  app.get('/data', getData(users))
+  app.get('/cities', getCities(users))
+  app.get('/users', getUsers(users))
+  app.get('/city/:name', getCity(users))
+  app.get('/user/:name', getUser(users))
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }
 
-const getData = (users) => (req, res) => {
-  res.send('No you!')
+const getCities = (users) => (req, res) => {
+  res.send('Cities!')
+}
+
+const getUsers = (users) => (req, res) => {
+  res.send('Users!')
+}
+
+const getCity = (users) => (req, res) => {
+  res.send('City!')
+}
+
+const getUser = (users) => (req, res) => {
+  res.send('User!')
 }
 
 MongoClient.connect(url).then(setup).catch(e => console.log(e))
