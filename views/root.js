834a7080aff3545e97b5acb70cc6b94d62543507
Matthew Bebis <matthewbebis@gmail.com>
2019-04-04 15:53:33 +0000

    rearranged js scripts

--- views/root.js
+++ views/root.js
@@ -92,10 +92,12 @@ const mapLanguGeImgs = () => {
 
 $( document ).ready(function() {
     console.log( "ready!" );
+    initMap()
+    displayMarkers()
+    
     addCities()
     mapLanguGeImgs()
     $('.collapsible').collapsible();
-
     addLanguages()
 });
 
@@ -127,4 +129,48 @@ const addLanguages = () => {
     console.log(citiesData)
 
 }
-
+  
+  const locations = ['toronto', 'montreal']
+  
+  var map
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 43.653, lng: -79.383 },
+      zoom: 8
+    })
+  }
+  
+  const getLocationsCoords = location => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyDIugBS6uYAKtUfNwqn7gqL6JnlIpAKeSQ`
+    )
+      .then(function(res) {
+        return res.json()
+      })
+      .then(function(json) {
+          console.log(json.results[0].geometry.location)
+        var marker = new google.maps.Marker({
+          position: json.results[0].geometry.location,
+          map: map,
+          label: {
+            color: 'black',
+            fontWeight: 'bold',
+            text: 'LANG'
+          },
+          icon: {
+            labelOrigin: new google.maps.Point(11, 50),
+            size: new google.maps.Size(22, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(11, 40)
+          }
+        })
+      })
+      .catch(err => console.error(err))
+  }
+  
+  const displayMarkers = () => {
+    locations.forEach(address => {
+      getLocationsCoords(address)
+    })
+  }
+  
